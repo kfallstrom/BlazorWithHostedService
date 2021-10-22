25398093496f29d96f3065b51c0f8ea5a724483f
Karl Fallstrom <karl.fallstrom@guidantfinancial.com>
2021-10-22 11:16:32 -0700

    baseline

---
+++ BlazorWithHostedService/Data/CacheWorker.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BlazorWithHostedService.Data
+{
+    public class CacheWorker : BackgroundService
+    {
+        private readonly ILogger<CacheWorker> _logger;
+        public IBackgroundTaskQueue _taskQueue { get; }
+
+        public CacheWorker(IBackgroundTaskQueue taskQueue,
+            ILogger<CacheWorker> logger)
+        {
+            _taskQueue = taskQueue;
+            _logger = logger;
+        }
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+                    _logger.LogInformation(
+            $"Queued Hosted Service is running.{Environment.NewLine}" +
+            $"{Environment.NewLine}Tap W to add a work item to the " +
+            $"background queue.{Environment.NewLine}");
+
+            await BackgroundProcessing(stoppingToken);
+        }
+
+        private async Task BackgroundProcessing(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                var workItem =
+                    await _taskQueue.DequeueAsync(stoppingToken);
+
+                try
+                {
+                    _logger.LogInformation(workItem.Name, workItem);
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex,
+                        "Error occurred executing {WorkItem}.", nameof(workItem));
+                }
+            }
+        }
+
+        public override async Task StopAsync(CancellationToken stoppingToken)
+        {
+            _logger.LogInformation("Queued Hosted Service is stopping.");
+
+            await base.StopAsync(stoppingToken);
+        }
+    }
+}
---
+++ BlazorWithHostedService/Models/GetQuoteModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorWithHostedService.Models
+{
+    public class GetQuoteModel
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
